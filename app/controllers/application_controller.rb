f6b9f0563008aab31b2c7590efe50a66cd1edf36
nickthecook <nickthecook@gmail.com>
2024-03-11 11:08:54 -0400

    some css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user = current_user
+  end
 end
