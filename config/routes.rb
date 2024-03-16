25e32588f868903ee9af4c46fa8d5495be5f1e96
nickthecook <nickthecook@gmail.com>
2024-03-16 08:36:58 -0400

    make collections the default page

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "conversations#index"
+  root "collections#index"
 end
