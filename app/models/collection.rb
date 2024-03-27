a6b4f770668bda4f30fcd1be397783e136a601ce
nickthecook <nickthecook@gmail.com>
2024-03-27 12:57:53 -0400

    destroy convo associations on destroy collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,6 @@
 class Collection < ApplicationRecord
   has_many :documents, dependent: :destroy
+  has_many :conversation_collections, dependent: :destroy
 
   def generate_slug
     update!(slug: "#{id}-#{name.parameterize}")
