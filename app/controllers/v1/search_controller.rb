14a01e8827bf0eaa30084fc3397e06f3e79fcd0c
oxaronick <nicholas.newberry@oxaro.com>
2024-04-10 10:48:27 -0400

    return better error message from /v1/search

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -5,6 +5,10 @@ module V1
       return render json: { "error": "No query given" }, status: :bad_request if query.blank?
 
       render json: { hits: Api::SearchMultiple.new(@client.collections, base_url: request.base_url).search(query) }
+    rescue StandardError => e
+      Rails.logger.error "Error while searching for #{query}: #{e}"
+
+      render json: { error: e.message }, status: :internal_server_error
     end
   end
 end
