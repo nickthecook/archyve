8794d848295a57d36541087727d7becc607055e6
oxaronick <nicholas.newberry@oxaro.com>
2024-05-07 16:54:55 -0400

    set up some settings mgmt in motor admin

--- app/models/model_config.rb
+++ app/models/model_config.rb
@@ -1,4 +1,6 @@
 class ModelConfig < ApplicationRecord
+  class ModelTypeError < StandardError; end
+
   belongs_to :model_server
   has_many :messages, as: :author, dependent: :destroy
 
@@ -7,5 +9,23 @@ class ModelConfig < ApplicationRecord
 
   def description
     "#{name}@#{model_server.name}"
+  end
+
+  def make_active_embedding_model
+    raise ModelTypeError, "Model is not an embedding model" unless embedding?
+
+    Setting.find_by(key: "embedding_model").update!(value: id)
+  end
+
+  def make_default_chat_model
+    raise ModelTypeError, "Model is an embedding model" if embedding?
+
+    Setting.find_by(key: "chat_model").update!(value: id)
+  end
+
+  def make_active_summarization_model
+    raise ModelTypeError, "Model is an embedding model" if embedding?
+
+    Setting.find_by(key: "summarization_model").update!(value: id)
   end
 end
