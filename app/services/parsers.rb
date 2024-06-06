552ce1e5d1da8801814922d432f54880a8cd5fa3
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-06-06 16:27:45 -0400

    Support ingesting DOCX files using pandoc

--- app/services/parsers.rb
+++ app/services/parsers.rb
@@ -2,6 +2,7 @@ module Parsers
   def self.parser_for(filename)
     name_locase = filename.downcase
     return Parsers::Pdf if name_locase.end_with?(".pdf")
+    return Parsers::Docx if name_locase.end_with?(".docx")
     return Parsers::Text if name_locase.end_with?(".txt", ".html", ".md")
 
     raise StandardError, "Unsupported file extension: '#{filename.slice(/\.\w+$/)}'"
