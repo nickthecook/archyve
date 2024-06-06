5960521404e0a769abf7b73da3c7af8bce8194d4
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-06-06 09:53:13 -0400

    Check status and log stderr if command fails

--- app/services/parsers/pdf.rb
+++ app/services/parsers/pdf.rb
@@ -11,8 +11,12 @@ module Parsers
       #       may cause tables to be "pretty" in text which may not be ideal for chunking.
       #       Not specifying works best for rotated pages, so doing that for now
       cmd = 'pdftotext - -'
-      txt, = Open3.capture2(cmd, stdin_data: @document.contents, binmode: true)
-      txt
+      txt, serr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
+      return txt if status.success?
+
+      Rails.logger.error("Error running '#{cmd}' on PDF: #{@document.filename}\n#{serr}")
+
+      raise StandardError, "Error converting PDF to text: #{@document.filename}'"
     end
   end
 end
