7ba9d26424f411b2e8b567c9c2bf2c159741bc3d
Nicholas Newberry <nicholas.newberry@oxaro.com>
2024-06-25 08:50:34 -0400

    rename variable for clarity

--- app/services/parsers/pdf.rb
+++ app/services/parsers/pdf.rb
@@ -11,10 +11,10 @@ module Parsers
       #       may cause tables to be "pretty" in text which may not be ideal for chunking.
       #       Not specifying works best for rotated pages, so doing that for now
       cmd = 'pdftotext - -'
-      txt, serr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
+      txt, stderr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
       return txt if status.success?
 
-      Rails.logger.error("Error running '#{cmd}' on PDF: #{@document.filename}\n#{serr}")
+      Rails.logger.error("Error running '#{cmd}' on PDF: #{@document.filename}\n#{stderr}")
 
       raise StandardError, "Error converting PDF to text: #{@document.filename}'"
     end
