daec151e97762ff62f7665c03c6544e2d5661e0e
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-06-25 13:15:21 -0400

    Cleanup

--- app/services/the_ingestor.rb
+++ app/services/the_ingestor.rb
@@ -9,33 +9,42 @@ class TheIngestor
 
     @document.chunking!
     chonker.each_with_index do |chunk_record, index|
-      if index.zero?
-        # Temporary hack since parser is still a brute force chunker right now.
-        @document.chunked!
-        @document.embedding!
-      end
-
-      chunk = Chunk.new(document: @document, content: chunk_record.content,
-                        embedding_content: chunk_record.embedding_content)
-      chunk.save!
-
-      Rails.logger.info("Embedding chunk #{chunk.id} (#{index})")
-      embedding = embedder.embed(chunk.embedding_content)
-
-      ids = chromadb.add_documents(@collection_id, [chunk.content], [embedding])
-      chunk.update!(vector_id: ids.first)
+      prepare_and_embed_chunk(chunk_record, index)
     end
     Rails.logger.info("Got chunks from #{@document.filename}.")
 
     @document.embedded!
   rescue StandardError => e
-    Rails.logger.error("Error ingesting document #{@document.id}\n#{e.class.name}: #{e.message}#{e.backtrace.join("\n")}")
+    Rails.logger.error("Error ingesting document #{@document.id}\n#{exception_summary(e)}")
     @document.error!
 
     raise e
   end
 
   private
+
+  def exception_summary(err)
+    "#{err.class.name}: #{err.message}#{err.backtrace.join("\n")}"
+  end
+
+  def document_embedding!
+    # Temporary hack since parser is still a brute force chunker right now.
+    @document.chunked!
+    @document.embedding!
+  end
+
+  def prepare_and_embed_chunk(chunk_record, index)
+    document_embedding! if index.zero?
+    chunk = Chunk.new(
+      document: @document, content: chunk_record.content,
+      embedding_content: chunk_record.embedding_content)
+    chunk.save!
+
+    # Rails.logger.info("Embedding chunk #{chunk.id} (#{index})")
+    embedding = embedder.embed(chunk.embedding_content)
+    ids = chromadb.add_documents(@collection_id, [chunk.content], [embedding])
+    chunk.update!(vector_id: ids.first)
+  end
 
   def ensure_collection_exists
     if @document.created?
