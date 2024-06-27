c9f27c9d301a9031c55ced276e5ac54f7b86e311
Nicholas Newberry <nicholas.newberry@oxaro.com>
2024-06-27 13:53:19 -0400

    add collection spec

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Collection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Collection do
+  subject { create(:collection, name: "Crazy Harry's Discount Documents") }
+
+  describe "#generate_slug" do
+    before do
+      subject.generate_slug
+    end
+
+    it "generates a slug from the name" do
+      expect(subject.slug).to eq("#{subject.id}-crazy-harry-s-discount-documents")
+    end
+  end
 end
