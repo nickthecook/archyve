d8038868eb8394d67240926ba89692610bcaab40
Nicholas Newberry <nicholas.newberry@oxaro.com>
2024-06-27 14:57:42 -0400

    lint stuff on controller

--- app/controllers/conversation_collections_controller.rb
+++ app/controllers/conversation_collections_controller.rb
@@ -1,17 +1,23 @@
 class ConversationCollectionsController < ApplicationController
-  def create
-    @collection = current_user.collections.find_by(id: params[:collection_ids])
-    # TODO: lock this down to the user
-    @conversation = Conversation.find(params[:conversation_id])
+  before_action :set_conversation, :set_collection
 
+  def create
     @conversation.conversation_collections.destroy_all
-    if @collection
-      ConversationCollection.create!(collection: @collection, conversation: @conversation)
-    end
+    ConversationCollection.create!(collection: @collection, conversation: @conversation) if @collection
 
     respond_to do |format|
       format.turbo_stream { @conversation.update_form }
       format.html { redirect_to conversation_path(@conversation) }
     end
+  end
+
+  private
+
+  def set_conversation
+    @conversation = current_user.conversations.find(params[:conversation_id])
+  end
+
+  def set_collection
+    @collection = current_user.collections.find(params[:collection_ids]) if params[:collection_ids]
   end
 end
