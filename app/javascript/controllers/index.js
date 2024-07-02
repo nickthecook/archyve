230c9fea170b90fec865e459a2f4f6698039c721
Nicholas Newberry <nicholas.newberry@oxaro.com>
2024-07-02 09:55:24 -0400

    add copy button to message

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+import Clipboard from "stimulus-clipboard"
+application.register('clipboard', Clipboard)
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,10 +2,11 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "marked", to: "https://ga.jspm.io/npm:marked@12.0.0/lib/marked.esm.js"
 pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.9.0/es/index.js"
+pin "stimulus-clipboard" # @4.0.1
