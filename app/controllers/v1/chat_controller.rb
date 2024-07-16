8185ef33d06778c078ec340e3027238e61612c52
Nicholas Newberry <nicholas.newberry@oxaro.com>
2024-07-16 08:49:52 -0400

    return proper errors from v1/chat

--- app/controllers/v1/chat_controller.rb
+++ app/controllers/v1/chat_controller.rb
@@ -4,6 +4,8 @@ module V1
       return render json: { error: "No prompt given" }, status: :bad_request if prompt.blank?
 
       render json: Api::ChatResponse.new(prompt, model:, api_client: @client).respond, status: :ok
+    rescue StandardError => e
+      render json: { error: e }, status: :internal_server_error
     end
 
     private
