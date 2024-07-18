a5f5043a38f485b00756cdab92ab70646acaff49
Nicholas Newberry <nicholas.newberry@oxaro.com>
2024-07-18 09:23:24 -0400

    support add collections param to v1/chat

--- app/controllers/v1/chat_controller.rb
+++ app/controllers/v1/chat_controller.rb
@@ -3,12 +3,24 @@ module V1
     def chat
       return render json: { error: "No prompt given" }, status: :bad_request if prompt.blank?
 
-      render json: Api::ChatResponse.new(prompt, model:, augment:, api_client: @client).respond, status: :ok
+      render json: Api::ChatResponse.new(
+        prompt,
+        model:,
+        augment:,
+        collections:,
+        api_client: @client
+      ).respond, status: :ok
+    rescue ActiveRecord::RecordNotFound, Api::ModelNotFoundError => e
+      render json: { error: e }, status: :not_found
     rescue StandardError => e
       render json: { error: e }, status: :internal_server_error
     end
 
     private
+
+    def collections
+      chat_params[:collections]&.split(",")&.map { |id| Collection.find(id) }
+    end
 
     def augment
       chat_params[:augment] == "true"
@@ -23,7 +35,7 @@ module V1
     end
 
     def chat_params
-      params.slice(:prompt, :model, :augment)
+      @chat_params ||= params.slice(:prompt, :model, :augment, :collections)
     end
   end
 end
