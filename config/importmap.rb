af9851a26add7bb31943f56d0668cdcd2be762ed
Kumanan at Oxaro <100310311+oxaroky02@users.noreply.github.com>
2024-08-12 16:24:18 -0400

    Cleanup importmap and yarn a little (#57)
    * Remove yarn packages not needed
    
    * Pin marked via importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "marked", to: "https://ga.jspm.io/npm:marked@12.0.0/lib/marked.esm.js"
+pin "marked" # @12.0.1
 pin "highlight.js", to: "https://ga.jspm.io/npm:highlight.js@11.9.0/es/index.js"
 pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
