f93b382bef5f23380f66a9f108f694fbbd3a1923
nick <nicholas.newberry@oxaro.com>
2024-08-21 13:56:54 -0400

    show entities in tab inside collections#show

--- app/models/graph_entity.rb
+++ app/models/graph_entity.rb
@@ -5,6 +5,35 @@ class GraphEntity < ApplicationRecord
   belongs_to :collection
   has_one :user, through: :collection
 
+  scope :by_description_count, lambda {
+    joins(:descriptions).group("graph_entities.id").order(Arel.sql("count(*) desc"))
+  }
+
+  scope :by_relationship_count, lambda {
+    joins(:relationships_to).joins(:relationships_from).group("graph_entities.id").order(Arel.sql("count(*) desc"))
+  }
+
+  after_create_commit lambda {
+    broadcast_append_to(
+      :collections,
+      target: "collection_#{collection.id}_entities",
+      partial: "shared/entity",
+      locals: {
+        entity: self,
+      }
+    )
+  }
+  after_update_commit lambda {
+    broadcast_replace_to(
+      :collections,
+      target: "graph_entity_#{id}",
+      partial: "shared/entity",
+      locals: {
+        entity: self,
+      }
+    )
+  }
+
   def graph_node
     Nodes::Entity.find_by(attributes.slice(*entity_find_attrs))
   end
