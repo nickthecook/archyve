1653c73b65527d3f0dffa23be7a010605b920814
nick <nicholas.newberry@oxaro.com>
2024-08-26 08:48:55 -0400

    fix old migration that broke when Collection#state was added

--- db/migrate/20240409133954_add_embedding_model_to_collection.rb
+++ db/migrate/20240409133954_add_embedding_model_to_collection.rb
@@ -1,8 +1,14 @@
 class AddEmbeddingModelToCollection < ActiveRecord::Migration[7.1]
+  Collection
+
+  class Collection < ApplicationRecord
+    attribute :state, :string
+  end
+
   def change
     add_reference :collections, :embedding_model, null: true, foreign_key: { to_table: :model_configs }
 
-    Collection.where(embedding_model: nil).each { |c| c.update!(embedding_model: embedding_model) }
+    Collection.where(embedding_model_id: nil).each { |c| c.update!(embedding_model_id: embedding_model.id) }
 
     change_column_null :collections, :embedding_model_id, false
   end
