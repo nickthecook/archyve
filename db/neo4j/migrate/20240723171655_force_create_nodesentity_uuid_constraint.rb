d61ad6a9ad451b5c10933196a348cda59d3523c9
nick <nicholas.newberry@oxaro.com>
2024-08-26 11:42:08 -0400

    fix neo4j migrations

--- db/neo4j/migrate/20240723171655_force_create_nodesentity_uuid_constraint.rb
+++
@@ -1,9 +0,0 @@
-class ForceCreateNodesentityUuidConstraint < ActiveGraph::Migrations::Base
-  def up
-    add_constraint :"Nodes::entity", :uuid, force: true
-  end
-
-  def down
-    drop_constraint :"Nodes::entity", :uuid
-  end
-end
--- db/neo4j/migrate/20240822191746_force_create_nodes_entity_uuid_constraint.rb
+++
@@ -1,9 +0,0 @@
-class ForceCreateNodesEntityUuidConstraint < ActiveGraph::Migrations::Base
-  def up
-    add_constraint :"Nodes::Entity", :uuid, force: true
-  end
-
-  def down
-    drop_constraint :"Nodes::Entity", :uuid
-  end
-end
