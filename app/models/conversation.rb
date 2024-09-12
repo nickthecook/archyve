c9cf6f234aa57b3a85cf11db5a8bdcf52320e302
nick <nicholas.newberry@oxaro.com>
2024-09-12 08:59:19 -0400

    only confirm convo delete when messages exist

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,7 @@
 class Conversation < ApplicationRecord
   belongs_to :user
   belongs_to :model_config
-  has_many :messages, dependent: :destroy, counter_cache: true
+  has_many :messages, dependent: :destroy
   has_many :conversation_collections, dependent: :destroy
   has_many :collections, through: :conversation_collections, dependent: :destroy
   has_many :api_calls, as: :traceable, dependent: :destroy
