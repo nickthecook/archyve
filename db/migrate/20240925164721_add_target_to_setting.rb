6f715ed0a42aa6a8450e2d8b7b65bf7d9d0bd1cc
nick <nicholas.newberry@oxaro.com>
2024-09-25 13:09:06 -0400

    fix migration

--- db/migrate/20240925164721_add_target_to_setting.rb
+++ db/migrate/20240925164721_add_target_to_setting.rb
@@ -3,7 +3,7 @@ class AddTargetToSetting < ActiveRecord::Migration[7.1]
     add_reference :settings, :target, null: false, polymorphic: true, null: true
 
     Setting.all.each do |setting|
-      setting.update(target: setting.user, target_type: 'User') if setting.user
+      setting.update(target_id: setting.user_id, target_type: 'User') if setting.user_id.present?
     end
   end
 end
