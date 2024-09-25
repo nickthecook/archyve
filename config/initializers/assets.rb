f798fb451157f6fe2caec881ee57f741088fc35f
nick <nicholas.newberry@oxaro.com>
2024-09-25 13:14:54 -0400

    make bg images work in container

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,8 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/images/backgrounds/dark")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/images/backgrounds/light")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
--- config/initializers/background_images.rb
+++ config/initializers/background_images.rb
@@ -1,10 +1,10 @@
-dark_files = Dir.glob(Rails.root.join("app/assets/images/backgrounds/dark/*").to_s)
+dark_files = Dir.glob(Rails.public_path.join('backgrounds/dark/*').to_s)
 Rails.configuration.dark_backgrounds = dark_files.map do |file|
-  "/assets/backgrounds/dark/#{File.basename(file)}"
+  "/backgrounds/dark/#{File.basename(file)}"
 end
 
 # still need a way to tell if the app is in light mode
-light_files = Dir.glob(Rails.root.join("app/assets/images/backgrounds/light/*").to_s)
+light_files = Dir.glob(Rails.public_path.join('backgrounds/light/*').to_s)
 Rails.configuration.light_backgrounds = light_files.map do |file|
-  "/assets/backgrounds/light/#{File.basename(file)}"
+  "/backgrounds/light/#{File.basename(file)}"
 end
