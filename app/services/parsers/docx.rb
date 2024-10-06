57f93bbadcb2571a6c72e73e85c042cdada2e280
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-10-06 15:27:26 -0400

    update docx and pdf parsing

--- app/services/parsers/docx.rb
+++ app/services/parsers/docx.rb
@@ -1,12 +1,13 @@
 module Parsers
   # DOCX parser converts word documents into commonmark (with tables) markdown
   class Docx < CommonMark
-    def text
+    def initialize(document)
+      super(document)
       # NOTE: Using -raw opt causes text to be broken up a lot; but not using raw
       #       may cause tables to be "pretty" in text which may not be ideal for chunking.
       #       Not specifying works best for rotated pages, so doing that for now
       cmd = 'pandoc -f docx  --wrap=none --to=commonmark -'
-      txt, serr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
+      @text, serr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
       return txt if status.success?
 
       Rails.logger.error("Error running '#{cmd}' on DOCX: #{@document.filename}\n#{serr}")
--- app/services/parsers/pdf.rb
+++ app/services/parsers/pdf.rb
@@ -1,15 +1,15 @@
 module Parsers
   class Pdf < Text
-    def text
+    def initialize(document)
+      super(document)
       # NOTE: Using -raw opt causes text to be broken up a lot; but not using raw
       #       may cause tables to be "pretty" in text which may not be ideal for chunking.
       #       Not specifying works best for rotated pages, so doing that for now
       cmd = 'pdftotext - -'
-      txt, stderr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
-      return txt if status.success?
+      @text, stderr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
+      return if status.success?
 
       Rails.logger.error("Error running '#{cmd}' on PDF: #{@document.filename}\n#{stderr}")
-
       raise StandardError, "Error converting PDF to text: #{@document.filename}'"
     end
   end
