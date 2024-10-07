93d47143eaf84950e5bd56d1701e50f982c3956f
oxaronick <86964206+oxaronick@users.noreply.github.com>
2024-10-07 14:34:17 -0400

    add test CI action (#96)
    * add test CI action
    
    * no ruby setup
    
    * fix zeitwerk error
    
    * exclude e2e spec and install apt deps
    
    * sudon't
    
    * silent apt install
    
    * apt-get update
    
    * really exclude e2e specs
    
    * install pdftotext
    
    * fix failing test that CI found!
    
    * fix legit linter errors!
    
    ---------
    
    Co-authored-by: nick <nicholas.newberry@oxaro.com>

--- spec/services/embedder_spec.rb
+++ spec/services/embedder_spec.rb
@@ -6,28 +6,18 @@ RSpec.describe Embedder do
   let(:model_server) { create(:model_server, provider: "ollama", url: 'http://localhost:11434', active: false) }
   let(:model_config) { create(:model_config, name: "Nomic", model: "nomic-embed-text", model_server:, embedding: true) }
 
-  describe "#new" do
-    context "when no model config given" do
-      let(:model_config) { nil }
+  let(:client_helper) { instance_double(Helpers::ModelClientHelper, client:) }
+  let(:client) { instance_double(LlmClients::Ollama::Client, embed: embeddings) }
+  let(:embeddings) { { "embedding" => [1.0, 1.1, 1.2] } }
 
-      it "raises an error" do
-        expect { subject }.to raise_error(NameError)
-      end
-    end
+  before do
+    allow(Helpers::ModelClientHelper).to receive(:new).and_return(client_helper)
   end
 
   describe "#embed" do
-    context "when no ModelServer is configured" do
-      let(:model_server) { nil }
-
-      it "raises an error" do
-        expect { subject.embed("Why is the sky blue") }.to raise_error(NameError)
-      end
-    end
-
     context "when ModelServer is available" do
       it "returns an embedding for 'Why is the sky blue?'" do
-        expect(subject.embed("Why is the sky blue")).to be_a(Array)
+        expect(subject.embed("Why is the sky blue?")).to be_a(Array)
       end
     end
   end
