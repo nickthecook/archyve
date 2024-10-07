83d4cf89c1040a8d65e0e77907ee5c99dcfe7bd8
nick <nicholas.newberry@oxaro.com>
2024-10-07 14:32:21 -0400

    fix legit linter errors!

--- spec/services/embedder_spec.rb
+++ spec/services/embedder_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Embedder do
   let(:model_server) { create(:model_server, provider: "ollama", url: 'http://localhost:11434', active: false) }
   let(:model_config) { create(:model_config, name: "Nomic", model: "nomic-embed-text", model_server:, embedding: true) }
 
-  let(:client_helper) { instance_double(Helpers::ModelClientHelper, client: client)}
+  let(:client_helper) { instance_double(Helpers::ModelClientHelper, client:) }
   let(:client) { instance_double(LlmClients::Ollama::Client, embed: embeddings) }
   let(:embeddings) { { "embedding" => [1.0, 1.1, 1.2] } }
 
