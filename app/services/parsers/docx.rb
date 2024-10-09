725bc575a0fdd0dca5a8691c96c43aced7656dbc
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-10-09 09:22:34 -0400

    Clean to better match tweaks to HtmlViaMarkdown

--- app/services/parsers/docx.rb
+++ app/services/parsers/docx.rb
@@ -1,18 +1,19 @@
 module Parsers
   # DOCX parser converts word documents into commonmark (with tables) markdown
   class Docx < CommonMark
+    CMD = 'pandoc -f docx  --wrap=none --to=commonmark -'
+
     def initialize(document)
       super(document)
       # NOTE: Using -raw opt causes text to be broken up a lot; but not using raw
       #       may cause tables to be "pretty" in text which may not be ideal for chunking.
       #       Not specifying works best for rotated pages, so doing that for now
-      cmd = 'pandoc -f docx  --wrap=none --to=commonmark -'
-      @text, serr, status = Open3.capture3(cmd, stdin_data: @document.contents, binmode: true)
-      return if status.success?
+      @text, e, s = Open3.capture3(CMD, stdin_data: @document.contents, binmode: true)
+      return if s.success?
 
-      Rails.logger.error("Error running '#{cmd}' on DOCX: #{@document.filename}\n#{serr}")
-
-      raise StandardError, "Error converting DOCX to markdown: #{@document.filename}'"
+      error = e&.lines&.first || 'Unknown error running pandoc'
+      Rails.logger.error("Error running '#{CMD}' on DOCX: #{@document.filename}\n#{error}")
+      raise StandardError, "Error converting DOCX to markdown: #{@document.filename}: #{error}"
     end
   end
 end
