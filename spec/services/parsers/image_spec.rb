9dcd8736b9d147ea9304e29bf443a4348f467356
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-10-17 09:51:26 -0400

    fix linting

---
+++ spec/services/parsers/image_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe Parsers::Image do
+  subject { described_class.new(document) }
+
+  let(:chunking_method) { :basic_image }
+  let(:chunking_profile) { create(:chunking_profile, method: chunking_method, size: 0) }
+  let(:file) { fixture_file_upload("avatar.jpg", 'application/image') }
+  let(:document) { create(:document, state: :created, file:, chunking_profile:) }
+
+  context "when method is :basic_image" do
+    #it_behaves_like "all parsers"
+
+    it "succeeds basic chunking" do
+      expect(subject.chunks.count).to eq(1)
+    end
+  end
+end
