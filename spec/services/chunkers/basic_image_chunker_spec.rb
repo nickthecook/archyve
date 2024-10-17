e40dd9cf8d36990183018dce73e249c9686a1f2b
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-10-17 09:51:26 -0400

    fix linting

--- spec/services/chunkers/basic_image_chunker_spec.rb
+++ spec/services/chunkers/basic_image_chunker_spec.rb
@@ -1,17 +1,5 @@
 RSpec.describe Chunkers::BasicImageChunker do
-  subject { described_class.new(chunking_profile, Chunkers::InputType::JPG) }
-
-  let(:chunk_size) { 1 }
-  let(:chunk_overlap) { 0 }
-  let(:chunking_profile) do
-    ChunkingProfile.create!(
-      method: :basic_image,
-      size: chunk_size,
-      overlap: chunk_overlap
-    )
+  describe "success" do
+    pending "add some examples (or delete) #{__FILE__}"
   end
-
-  # TODO: Add something...  Should these work(?):
-  # it_behaves_like "all chunkers"
-  # it_behaves_like "chunkers supporting overlap"
 end
