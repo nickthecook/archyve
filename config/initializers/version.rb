f927a702eb5cb80d3854176c94829a4556baa4bd
nick <nicholas.newberry@oxaro.com>
2024-11-05 10:22:22 -0500

    release 1.20.0

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,6 @@
 Rails.configuration.version = if Dir.exist?('.git')
   `git describe --tags`.strip
 else
-  "1.15.0"
+  # make this calculated in production, or at least have tests or something else to detect drift from git tag
+  "v1.20.0"
 end
