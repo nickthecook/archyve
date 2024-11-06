77084ae0cca453c69517c29b6890703f5aa6b89c
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-11-06 11:44:50 -0500

    Use two newlines between prompt parts

--- app/services/chunkers/chunk_record.rb
+++ app/services/chunkers/chunk_record.rb
@@ -17,13 +17,13 @@ module Chunkers
       # vector embeddings
       text = ""
       if headings
-        text << headings << "\n"
+        text << headings << "\n\n"
       end
       if location_summary
-        text << location_summary << "\n"
+        text << location_summary << "\n\n"
       end
       # Use document excerpt if no surrounding content provided (compatibility)
-      text << (surrounding_content || excerpt) << "\n"
+      text << (surrounding_content || excerpt)
     end
   end
 end
