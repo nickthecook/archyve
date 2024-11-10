f4d6103fe3774d7fa98ebe10b047802987ca0f21
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-11-10 17:47:10 -0500

    Remove dead comments, avoid `after` block, and other cleanup based on review.

--- spec/services/mediator_spec.rb
+++ spec/services/mediator_spec.rb
@@ -11,31 +11,23 @@ RSpec.describe Mediator do
   let(:doc) { Document.new(state: :created, link:, file:, filename:, chunking_profile:, collection:, user:) }
 
   describe "#ingest document" do
-    after do
-      FetchWebDocumentJob.clear
-      ChunkDocumentJob.clear
-    end
-
     context "with web link" do
       let(:link) { "https://en.wikipedia.org/wiki/Tabloid_journalism" }
-      # let(:doc) { create(:document, state: :created, filename: nil, link: "https://en.wikipedia.org/wiki/Tabloid_journalism", chunking_profile:) }
 
       it "fetches web page" do
         expect do
           subject.ingest(doc)
         end.to change(FetchWebDocumentJob.jobs, :size).by(1)
+        Sidekiq::Worker.clear_all
       end
 
       context "when web page has been fetched" do
-        before do
-          allow(Document).to receive(:find).and_return(doc)
-        end
-
         it "chunks document" do
-          expect do
-            subject.ingest(doc)
-            FetchWebDocumentJob.drain
-          end.to change(ChunkDocumentJob.jobs, :size).by(1)
+          doc.save
+          subject.ingest(doc)
+
+          expect { FetchWebDocumentJob.drain }.to change(ChunkDocumentJob.jobs, :size).by(1)
+          Sidekiq::Worker.clear_all
         end
       end
     end
@@ -43,12 +35,12 @@ RSpec.describe Mediator do
     context "with filename" do
       let(:file) { fixture_file_upload("gnu_manifesto.md", 'application/html') }
       let(:filename) { "spec/fixtures/files/gnu_manifesto.md" }
-      # let(:doc) { create(:document, state: :created, filename: "spec/fixtures/files/gnu_manifesto.md", chunking_profile:) }
 
       it "chunks document" do
         expect do
           subject.ingest(doc)
         end.to change(ChunkDocumentJob.jobs, :size).by(1)
+        Sidekiq::Worker.clear_all
       end
     end
   end
