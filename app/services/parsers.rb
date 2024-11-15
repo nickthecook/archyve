6eac5f9fcf58b840b755df227488d6e2792a0c3f
Kumanan <kumanan.yogaratnam@rcgtconsulting.com>
2024-11-15 17:50:26 -0500

    Tweak Parser to be more consistent

--- app/services/parsers.rb
+++ app/services/parsers.rb
@@ -1,14 +1,15 @@
 module Parsers
   class UnsupportedFileFormat < StandardError; end
 
-  ENABLED = [Pdf, Docx, CommonMark, Text, HtmlViaMarkdown, Jpg].freeze
+  ENABLED = [Docx, CommonMark, Text, HtmlViaMarkdown].freeze
 
   # Returns true if the `content_type` refers to content that can be parsed into
   # text for chunking
-  def self.textual?(content_type)
-    content_type.start_with?("text/") ||
-      ["application/pdf", "application/html"].include?(content_type) ||
-      ["officedocument.word"].any? { |t| content_type.include?(t) }
+  def self.can_chunk?(document)
+    name_locase = document.filename.downcase
+    content_type = document.content_type
+
+    ENABLED.any? { |p| p.can_parse?(name_locase, content_type) }
   end
 
   def self.parser_for(filename, content_type = nil)
@@ -17,6 +18,6 @@ module Parsers
     parsers = ENABLED.select { |p| p.can_parse?(name_locase, content_type) }
     return parsers.first unless parsers.empty?
 
-    raise UnsupportedFileFormat, "Unsupported file extension: '#{filename.slice(/\.\w+$/)}'"
+    raise UnsupportedFileFormat, "Unsupported extension '#{filename.slice(/\.\w+$/)}', #{content_type}"
   end
 end
