cbc708829ba0c5babc329157baf5321784a8cdad
oxaronick <86964206+oxaronick@users.noreply.github.com>
2024-11-19 10:30:21 -0500

    Protect from more ollama responses (#142)
    * update version to 1.20.2
    
    * protect from ollama responses with nested hashes
    
    ---------
    
    Co-authored-by: nick <nicholas.newberry@oxaro.com>

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,5 +2,5 @@ Rails.configuration.version = if Dir.exist?('.git')
   `git describe --tags`.strip
 else
   # make this calculated in production, or at least have tests or something else to detect drift from git tag
-  "v1.20.0"
+  "v1.20.2"
 end
